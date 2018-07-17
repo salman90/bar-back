b51e7b4abcbaef9244748d8139b6014de138ccea
salman90 <salmansalem9022@gmail.com>
2018-07-17 13:17:00 -0700

    added firebase & auth with firebase with email and password

--- src/index.js
+++ src/index.js
@@ -2,19 +2,45 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CoctailList from './screens/cocktailList';
 import UserCocktail from './screens/userCockatil';
-import Profile from './screens/profile'
+import Profile from './screens/profile';
+import Auth from './screens/auth'
 import { createBottomTabNavigator } from 'react-navigation';
+import * as firebase from "firebase";
+
 
 
 class App extends React.Component {
 
+  componentWillMount(){
+    const config = {
+      apiKey: "AIzaSyCqIoVukz6TmYayMf-uFwDmxYPdOOTl6aM",
+      authDomain: "bar-back-c3947.firebaseapp.com",
+      databaseURL: "https://bar-back-c3947.firebaseio.com",
+      projectId: "bar-back-c3947",
+      storageBucket: "bar-back-c3947.appspot.com",
+      messagingSenderId: "133444454362"
+    };
+    firebase.initializeApp(config);
+
+  }
+
   render() {
     const MainNavigation = createBottomTabNavigator({
-      cocktailList: {screen: CoctailList},
-      profile: {screen: Profile},
-      UserCocktail: {screen: UserCocktail},
-    })
-    // const main =
+      auth: {screen: Auth },
+      main: {
+        screen: createBottomTabNavigator({
+          cocktailList: {screen: CoctailList},
+          profile: {screen: Profile},
+          usercocktail: {screen: UserCocktail }
+        })
+      }
+
+    },{
+      navigationOptions: {
+          tabBarVisible: false
+      },
+    },
+  )
     return (
       <MainNavigation />
     );
---
+++ src/screens/auth.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import {View, Text, TextInput, Dimensions, KeyboardAvoidingView, ActivityIndicator  } from 'react-native';
+import { Input, Card, Icon, Button } from 'react-native-elements';
+import * as firebase from "firebase";
+
+const {height, width} = Dimensions.get('window');
+
+class Auth extends Component {
+
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(userAuth => {
+      if(userAuth){
+        // this.setState({ loggedIn: true})
+        this.props.navigation.navigate('cocktailList')
+      }
+    })
+  }
+ state = {
+   email: '',
+   password: '',
+   loading: false
+ }
+
+  onEmailChange = (text) =>  {
+    this.setState({ email: text })
+  }
+
+  onPasswordChange = (text) => {
+    this.setState({ password: text })
+  }
+
+  signInUser = () => {
+    this.setState({ loading: true })
+    const { email, password } = this.state
+    firebase.auth().signInWithEmailAndPassword(email, password)
+     .then(() => {
+       this.setState({ error: '', loading: false });
+       this.props.navigation.navigate('cocktailList')
+    })
+     .catch(() => {
+       firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(() => {
+          this.setState({error: '' ,loading: false})
+          this.props.navigation.navigate('cocktailList')
+        })
+        .catch((error) => {
+          this.setState({error: '' ,loading: false})
+          let errorCode = error.code;
+              let errorMessage = error.message;
+              if (errorCode == 'auth/weak-password') {
+                alert('The password is too weak.');
+              } else {
+                alert(errorMessage);
+              }
+        })
+     })
+  }
+
+  render(){
+    if(this.state.loading){
+      return (
+        <View
+         style={styles.container}
+        >
+        <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )
+    }else{
+      return(
+        <KeyboardAvoidingView
+           style={{flex: 1}}
+           behavior="padding"
+        >
+        <View
+         style={styles.container}
+        >
+          <View
+           style={styles.emailContainer}
+          >
+            <Icon
+             name='user'
+             type='font-awesome'
+             containerStyle={{ padding: 10}}
+            />
+            <TextInput
+             style={styles.input}
+             placeholder='Email'
+             autoCorrect={false}
+             onChangeText={(text) => this.onEmailChange(text)}
+             autoCapitalize='none'
+             underlineColorAndroid='rgba(0,0,0,0)'
+            />
+
+          </View>
+          <View
+            style={styles.passwordContianer}
+          >
+            <Icon
+            name= 'unlock'
+            type='font-awesome'
+            containerStyle={{ padding: 10}}
+            />
+            <TextInput
+             style={styles.input}
+             placeholder='password'
+             autoCorrect={false}
+             secureTextEntry={true}
+             onChangeText={(text) => this.onPasswordChange(text)}
+             underlineColorAndroid='rgba(0,0,0,0)'
+            />
+          </View>
+          <View
+          style={{ alignItems: 'center', justifyContent: 'center', marginTop: 10}}
+          >
+             <Button
+              buttonStyle={{ width: 250, borderRadius: 10}}
+              title='Sign Up'
+              onPress={this.signInUser}
+             />
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+      )
+    }
+  }
+}
+
+const styles = {
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },emailContainer:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },passwordContianer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10
+  }, input: {
+     flex: 1,
+     paddingTop: 10,
+     marginRight: 10,
+     paddingBottom: 10,
+     paddingLeft:0 ,
+     borderBottomWidth: 2,
+     borderColor: 'gray',
+  },
+}
+
+
+export default Auth
