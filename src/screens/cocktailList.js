a6e1ceb1777f50c24c5ddecefd078a2aa4579512
Torri Porter <torri.porter@yahoo.com>
2018-07-17 18:20:18 -0700

    list progress

--- src/screens/cocktailList.js
+++ src/screens/cocktailList.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import  {View, Text } from 'react-native';
+import  {
+  View,
+  Text,
+  ScrollView,
+  TouchableHighlight,
+} from 'react-native';
+import * as firebase from 'firebase';
+// import Search from 'react-native-searchbar';
+import fontAwesome from 'react-native-vector-icons';
 // import { createBottomTabNavigator } from 'react-navigation';
-
-
 class CoctailList extends Component {
+  state = {
+    cocktailNames: []
+  }
+  componentWillMount(){
+    firebase.database().ref('/cocktail_list').on('value', (snapshot) =>{
+      const cocktailList = snapshot.val()
+      this.setState({ cocktailNames: Object.keys(cocktailList) })
+    });
+  }
   render(){
     return(
-      <View
-       style={{ alignItems: 'center', justifyContent: 'center', flex: 1}}
-      >
-       <Text>Coctail List</Text>
-      </View>
-    )
+      <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'teal'}}>
+        <ScrollView style={{paddingTop:70}}>
+        {this.state.cocktailNames.length !== 0 ?
+      this.state.cocktailNames.map((name, i) => {
+        return (
+          <TouchableHighlight
+           key={i}
+          >
+            <Text key={i}>
+             {name}
+            </Text>
+          </TouchableHighlight>
+          );
+         })
+          :
+        <Text>No cocktails</Text>
+        }
+    </ScrollView>
+  </View>
+    );
   }
 }
-
 export default CoctailList;
