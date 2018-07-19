57bcddb0500682a227fec0c234dc0c0e836f6e11
Torri Porter <torri.porter@yahoo.com>
2018-07-19 15:02:21 -0700

    details page added

---
+++ src/screens/cocktailDetail.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import  {
+  View,
+  Text,
+  Button,
+  ScrollView,
+  TouchableHighlight,
+} from 'react-native';
+import * as firebase from 'firebase';
+
+class CocktailDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cocktail: {}
+    };
+  }
+  componentWillMount(){
+    firebase.database().ref('/cocktail_list/' + `${this.props.navigation.state.params.cocktail}`).on('value', (snapshot) =>{
+      this.setState({cocktail: snapshot.val()});
+    });
+  }
+  render(){
+    const { params } = this.props.navigation.state;
+    const back = () => {
+      this.props.navigation.goBack(null);
+    }
+    return (
+      <View style={{paddingTop: 50}}>
+        <Text>Details on {this.state.cocktail.name}</Text>
+      </View>
+    )
+  }
+}
+
+export default CocktailDetail;
